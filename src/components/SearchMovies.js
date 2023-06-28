b01662737415ec2a32a10b96622209f9e1152aa6
Luchito30 <luchodick24@gmail.com>
2023-06-28 00:19:33 -0300

    termino tp

---
+++ src/components/SearchMovies.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function SearchMovies() {
+  const [movies, setMovies] = useState([]);
+  const [keyword, setKeyword] = useState('');
+
+  // Credenciales de API
+  const apiKey = 'http://www.omdbapi.com/?i=tt3896198&apikey=d0c85413';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiKey}&s=${keyword}`);
+        const data = await response.json();
+        if (data.Search) {
+          setMovies(data.Search);
+        } else {
+          setMovies([]);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (keyword !== '') {
+      fetchData();
+    }
+  }, [apiKey, keyword]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const keywordValue = inputRef.current.value;
+    setKeyword(keywordValue);
+  };
+
+  const inputRef = useRef(null);
+
+  return (
+    <div className="container-fluid">
+      {apiKey !== '' ? (
+        <>
+          <div className="row my-4">
+            <div className="col-12 col-md-6">
+              {/* Buscador */}
+              <form method="GET" onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="">Buscar por título:</label>
+                  <input type="text" className="form-control" ref={inputRef} />
+                </div>
+                <button className="btn btn-info">Search</button>
+              </form>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <h2>Películas para la palabra: {keyword}</h2>
+            </div>
+            {/* Listado de películas */}
+            {movies.length > 0 ? (
+              movies.map((movie, i) => (
+                <div className="col-sm-6 col-md-3 my-4" key={i}>
+                  <div className="card shadow mb-4">
+                    <div className="card-header py-3">
+                      <h5 className="m-0 font-weight-bold text-gray-800">{movie.Title}</h5>
+                    </div>
+                    <div className="card-body">
+                      <div className="text-center">
+                        <img
+                          className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                          src={movie.Poster}
+                          alt={movie.Title}
+                          style={{ width: '90%', height: '400px', objectFit: 'cover' }}
+                        />
+                      </div>
+                      <p>{movie.Year}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="col-12">
+                <div className="alert alert-warning text-center">No se encontraron películas</div>
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
+        <div className="alert alert-danger text-center my-4 fs-2">Eyyyy... ¿PUSISTE TU APIKEY?</div>
+      )}
+    </div>
+  );
+}
+
+export default SearchMovies;
